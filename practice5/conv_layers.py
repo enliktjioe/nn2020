364077e3119caa51dd4917b493bc333d9ef1f757
Enlik - <enlik@ut.ee>
2020-03-31 23:54:18 +0300

    Finished task 2.1

--- practice5/conv_layers.py
+++ practice5/conv_layers.py
@@ -30,7 +30,43 @@ def conv_forward_naive(x, w, b, conv_param):
     # TODO: Implement the convolutional forward pass.                         #
     # Hint: you can use the function np.pad for padding.                      #
     ###########################################################################
-    pass
+    # Get the dimensions of the input data x
+    N, C, H, W = x.shape
+
+    # Get the dimensions of the filter weights
+    F, _, HH, WW = w.shape
+
+    # Get the stride and pad parameters
+    stride = conv_param['stride']
+    padding = conv_param['pad']
+
+    # Compute the output volume size of the hidden layer and the weights
+    out_H = 1 + ((H + 2 * padding - HH) / stride)
+    out_W = 1 + ((H + 2 * padding - WW) / stride)
+
+    # Initialize the output container
+    out = np.zeros((N, F, int(out_H), int(out_W)))
+
+    # Pad the input data starting at position (1,1) and using zero constant values
+    pad_input = np.zeros((N, C, H + 2 * padding, W + 2 * padding))
+    for i in range(N):
+        for j in range(C):
+            pad_input[i, j] = np.pad(x[i, j], (1, 1), 'constant', constant_values=(0, 0))
+
+    # Process the loop through the layer and then apply the filter
+    for i in range(N):
+        for j in range(int(out_H)):
+            for k in range(int(out_W)):
+                for l in range(F):
+                    # Get the input values to be convoluted
+                    x_now = pad_input[i, :, j * stride:j * stride + HH, k * stride:k * stride + WW]
+                    # Get the indexed filter
+                    kernel = w[l]
+                    # Convolve the input data using the indexed filter
+                    out[i, l, j, k] = np.sum(x_now * kernel)
+                # When convolution is done, add the bias
+                out[i, :, j, k] = out[i, :, j, k] + b
+
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
